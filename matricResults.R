81399e5bbcd0b715ae82222d66dfe7dee18dc558
Adam <adam@adamoxford.co.uk>
2022-01-13 11:57:18 +0200

    first commit

---
+++ matricResults.R
@@ -0,0 +1,51 @@
+#install.packages("tidyverse")
+#install.packages("leaflet")
+#install.packages("leafgl")
+
+library("tidyverse")
+library("leaflet")
+library("dplyr")
+library("leafgl")
+library("leaflet.extras")
+
+schoolsData <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQRGIMjdnID_DgFPq9JT0KFql7TxngN7_u5IR3ji86aCdqm4_BEhtcxYMiiClXNSfl4zcQNpvhiGNa_/pub?gid=1989311314&single=true&output=csv")
+
+schoolsImproved <- schoolsData %>% filter(X..Change >= 0)
+schoolsWorse <-schoolsData %>% filter(X..Change < 0)
+
+pal <- colorQuantile(palette = "RdYlGn", domain = schoolsImproved$X2020....Pass)
+pal1 <- colorQuantile(palette = "RdYlGn", domain = schoolsWorse$X2020....Pass)
+
+m <- leaflet(options = leafletOptions(preferCanvas = FALSE)) %>%
+  setView(lng = 24.47, lat = -28.47, zoom = 6)%>%
+  addProviderTiles(providers$Stamen.TonerLite)%>%
+  addCircleMarkers(data = schoolsImproved, lng = as.numeric(schoolsImproved$Longitude), lat = as.numeric(schoolsImproved$Latitude), popup = paste(
+                                                                                                          "<b>School</b>:", schoolsImproved$School.name,
+                                                                                                          "<br/><b>Quintile</b>:", schoolsImproved$Quintile, 
+                                                                                                          "<br/><b>Pass rate 2020</b>:", schoolsImproved$X2020....Pass,
+                                                                                                          "<br/><b>Change y-o-y</b>", schoolsImproved$X..Change), 
+                  radius = schoolsImproved$Quintile, color = ~pal(schoolsImproved$X2020....Pass), group = "Improved") %>%
+  addCircleMarkers(data = schoolsWorse, lng = as.numeric(schoolsWorse$Longitude), lat = as.numeric(schoolsWorse$Latitude), popup = paste(
+    "<b>School</b>:", schoolsWorse$School.name,
+    "<br/><b>Quintile</b>:", schoolsWorse$Quintile, 
+    "<br/><b>Pass rate 2020</b>:", schoolsWorse$X2020....Pass,
+    "<br/><b>Change y-o-y</b>", schoolsWorse$X..Change), 
+    radius = schoolsWorse$Quintile, color = ~pal1(schoolsWorse$X2020....Pass), group = "Worse") %>%
+  addLayersControl(overlayGroups = c("Improved","Worse"), 
+                   options = layersControlOptions(collapsed = FALSE))%>%
+
+#  addSearchOSM(options = searchOptions(hideMarkerOnCollapse = TRUE, zoom = 7)) 
+  
+ addSearchFeatures(
+ targetGroups = c("Improved","Worse"),
+ options = searchFeaturesOptions(
+  zoom = 10, openPopup = TRUE, firstTipSubmit = TRUE,
+ autoCollapse = TRUE, hideMarkerOnCollapse = TRUE ))
+
+m
+
+#  addCircleMarkers(lng = schoolsData$New.Long, lat = schoolsData$New.Lat, popup = schoolsData$Institution_Name, color = schoolsData$Quintile)
+
+m <- leaflet(options = leafletOptions(preferCanvas = TRUE)) %>%
+  addTiles()%>%
+  addMarkers(lng=30.33409, lat=-23.67264)
